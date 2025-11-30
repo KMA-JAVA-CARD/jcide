2885e610372402d9d54ce65d737facd42b8d1f7c
sondoannam <sondoannam202@gmail.com>
2025-11-30 21:02:04 +0700

    feat: :sparkles: update

--- demo.java
+++ demo.java
@@ -1,32 +1,56 @@
 package demo; 
-import javacard.framework.*; 
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.OwnerPIN;
+import javacard.framework.Util;
+import javacard.security.*;
+import javacardx.crypto.*;
 
-public class demo extends Applet {     
-    // --- KHAI B¡O C¡C LNH (INS) ---
+public class demo extends Applet {
     final static byte CLA_APPLET = (byte) 0xA0;     
     final static byte INS_REGISTER = (byte) 0x01; 
     final static byte INS_VERIFY = (byte) 0x02;   
     final static byte INS_GET_INFO = (byte) 0x03; 
-    final static byte INS_CHANGE_PIN = (byte) 0x04; // <--- TH M LNH N¿Y
-    final static byte INS_UNBLOCK_PIN = (byte) 0x05; // (T˘y chn: M khÛa th)
+    final static byte INS_CHANGE_PIN = (byte) 0x04;
+    final static byte INS_UNBLOCK_PIN = (byte) 0x05;
+    final static byte INS_GET_CARD_ID = (byte) 0x06;
 
-    // Cu hÏnh PIN
     final static byte PIN_TRY_LIMIT = (byte) 0x03; 
     final static byte MAX_PIN_SIZE = (byte) 0x06;  
     
     private OwnerPIN pin;     
     private byte[] userData; 
     private short userDataLen; 
-    final static short MAX_DATA_SIZE = (short) 256;     
+    final static short MAX_DATA_SIZE = (short) 256;
+    
+    // Khai b√°o bien RSA v√† luu ID the
+    private byte[] cardID; // Luu ID ngau nhi√™n (8 bytes)
+    private KeyPair keyPair;
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;     
 
     public static void install(byte[] bArray, short bOffset, byte bLength) {         
         new demo().register(bArray, (short) (bOffset + 1), bArray[bOffset]);     
     }     
 
-    protected demo() {         
-        pin = new OwnerPIN(PIN_TRY_LIMIT, MAX_PIN_SIZE);                  
+    protected demo() {
+		pin = new OwnerPIN(PIN_TRY_LIMIT, MAX_PIN_SIZE);                  
         userData = new byte[MAX_DATA_SIZE];         
-        userDataLen = 0;     
+        userDataLen = 0;
+        cardID = new byte[8];
+        
+        // Khoi tao KeyPair RSA (D√πng 1024 bit cho nh th gi lp)
+        // Nu th tht mnh, c√≥ th i l√™n LENGTH_RSA_2048
+        try {
+            keyPair = new KeyPair(KeyPair.ALG_RSA, KeyBuilder.LENGTH_RSA_1024);
+            publicKey = (RSAPublicKey) keyPair.getPublic();
+            privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        } catch (CryptoException e) {
+            // X l√Ω li nu th kh√¥ng h tr RSA (thng th JCOP u h tr)
+            ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+        } 
     }     
 
     public void process(APDU apdu) {         
@@ -47,7 +71,7 @@ public class demo extends Applet {
             case INS_GET_INFO:                 
                 getInfo(apdu);                 
                 break;
-            case INS_CHANGE_PIN:  // <--- TH M CASE N¿Y
+            case INS_CHANGE_PIN:
                 changePin(apdu);
                 break;
             case INS_UNBLOCK_PIN:
@@ -57,51 +81,84 @@ public class demo extends Applet {
                 ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);         
         }     
     }     
+    
+    // c ID
+    private void getCardID(APDU apdu) {
+        apdu.setOutgoing();
+        apdu.setOutgoingLength((short) 8);
+        apdu.sendBytesLong(cardID, (short) 0, (short) 8);
+    }
 
-    // ... (Gi nguyÍn registerUser, verifyPin, getInfo c ca bn) ...
-    // Copy li on code c ca 3 h‡m trÍn v‡o ‚y.
-    // Lu ˝: Trong h‡m registerUser c ca bn, nh gi nguyÍn logic cp nht PIN v‡ Data.
-
-    // --- TH M H¿M MI: I PIN ---
     private void changePin(APDU apdu) {
-        // Bt buc phi Verify PIN c th‡nh cÙng trc mi cho i
         if (!pin.isValidated()) {
              ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
         }
 
         byte[] buf = apdu.getBuffer();
-        byte len = (byte)apdu.setIncomingAndReceive(); // Nhn PIN mi
+        byte len = (byte)apdu.setIncomingAndReceive();
         
         if (len > MAX_PIN_SIZE || len < 1) {
             ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
         }
 
-        // Cp nht PIN mi (Ly d liu t v trÌ CDATA)
         pin.update(buf, ISO7816.OFFSET_CDATA, len);
     }
     
-    // (T˘y chn) Reset PIN v mc nh 123456 nu th b khÛa (d˘ng cho Demo cho d)
     private void resetPin(APDU apdu) {
-        // Trong thc t cn m„ PUK,  ‚y demo mÏnh reset luÙn
         byte[] defaultPIN = {(byte)0x31, (byte)0x32, (byte)0x33, (byte)0x34, (byte)0x35, (byte)0x36};
         pin.update(defaultPIN, (short)0, (byte)6);
         pin.resetAndUnblock();
     }
     
-    // ... Copy li h‡m verifyPin, getInfo, registerUser c v‡o ‚y ...
-    //  code gn, tÙi vit li registerUser, verifyPin, getInfo  di  bn d copy full nu cn:
-    
     private void registerUser(APDU apdu) {         
         byte[] buf = apdu.getBuffer();         
         short len = apdu.setIncomingAndReceive(); 
+
+		// -- Lu th√¥ng tin th --
         byte pinLen = buf[ISO7816.OFFSET_CDATA];                  
-        if (pinLen > MAX_PIN_SIZE || pinLen <= 0) ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);         
-        pin.update(buf, (short)(ISO7816.OFFSET_CDATA + 1), pinLen);                  
+        if (pinLen > MAX_PIN_SIZE || pinLen <= 0) ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+                
+        // Cp nht PIN
+        pin.update(buf, (short)(ISO7816.OFFSET_CDATA + 1), pinLen);
+        
+		// Lu ttin user
         short dataOffset = (short)(ISO7816.OFFSET_CDATA + 1 + pinLen);         
         short dataLen = (short)(len - 1 - pinLen);                  
         if (dataLen > MAX_DATA_SIZE) ISOException.throwIt(ISO7816.SW_FILE_FULL);         
         Util.arrayCopy(buf, dataOffset, userData, (short)0, dataLen);         
-        userDataLen = dataLen; 
+        userDataLen = dataLen;
+        
+        // 2. T sinh Card ID ngu nhi√™n (8 bytes)
+        RandomData rng = RandomData.getInstance(RandomData.ALG_PSEUDO_RANDOM);
+        rng.generateData(cardID, (short) 0, (short) 8);
+        
+        // Sinh kh√≥a RSA v√† Tr v Public Key
+        keyPair.genKeyPair(); // tn thi gian nht
+        
+        // -- Chun b d liu tr v --
+        
+        short outOffset = 0;
+        
+        // Copy CardID v√†o buffer
+        Util.arrayCopy(cardID, (short) 0, buf, outOffset, (short) 8);
+        outOffset += 8;
+        
+        // Copy Modulus
+        short modLen = publicKey.getModulus(buf, (short)(outOffset + 2));
+        Util.setShort(buf, outOffset, modLen); // Ghi  d√†i Mod
+        outOffset += 2;
+        outOffset += modLen;
+        
+        // Copy Exponent
+        short expLen = publicKey.getExponent(buf, (short)(outOffset + 2));
+        Util.setShort(buf, outOffset, expLen); // Ghi  d√†i Exp
+        outOffset += 2;
+        outOffset += expLen;
+
+        // Gi tt c v Swing
+        apdu.setOutgoing();
+        apdu.setOutgoingLength(outOffset);
+        apdu.sendBytes((short)0, outOffset);
     }     
 
     private void verifyPin(APDU apdu) {         
